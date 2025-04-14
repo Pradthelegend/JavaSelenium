55bde68f0184d4adf195b886e4e529cb5274bbb2
Pradeep R <pradeep.r@ibm.com>
2025-04-14 22:40:05 +0530

    Nothing New

---
+++ src/main/java/com/practicetestautomation/pageobjects/ExceptionsPage.java
@@ -0,0 +1,10 @@
+package com.practicetestautomation.pageobjects;
+
+import org.openqa.selenium.By;
+
+public class ExceptionsPage {
+
+    private By addButtonLocator = By.xpath("//button[text()='Add']");
+    private By row2InputfieldLocator = By.xpath("//div[@id='row2']/input[@class='input-field']");
+
+}
